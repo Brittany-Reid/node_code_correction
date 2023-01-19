c3507106c17db04b75c0580431e8d9997a4c933b
Brittany Reid <39616756+Brittany-Reid@users.noreply.github.com>
2023-01-19 20:00:30 +1030

    analysis for ts-fixes

--- example.js
+++ example.js
@@ -28,26 +28,32 @@ var snippets = [
           return true;
         }
         const response = await prompts(questions, { onCancel });
-      })();`
+      })();`,
+  `assert(true)`,
+  `import sum from './sum';\n\nit('sums numbers', () => {\n  expect(sum(1, 2)).toEqual(3);\n  expect(sum(2, 2)).toEqual(4);\n});`,
+  `import sum from './sum';
+
+  expect(sum(1, 2)).toEqual(3)
+  `
 ];
 
 async function main(){
-    var i = 1;
+    var i = 5;
     var compiled = await NCC.getErrors(snippets);
     console.log(compiled[i].errors)
     
 
+    NCC.evaluator.fixer.deletions = false;
     var fixed = await NCC.fix(snippets);
-    console.log(fixed[i].code)
+    console.log(fixed[i])
+    console.log(fixed[i].errors)
 
-    var e = new Evaluator();
-    var r = e.errors(snippets.map(s => {return new Snippet(s)}))
-    console.log(r[1].errors)
-    var r = e.fix(snippets.map(s => {return new Snippet(s)}))
-    console.log(r[1].code)
+    // var e = new Evaluator();
+    // var r = e.errors(snippets.map(s => {return new Snippet(s)}))
+    // console.log(r[1].errors)
+    // var r = e.fix(snippets.map(s => {return new Snippet(s)}))
+    // console.log(r[1].code)
 }
 
-var s = ts.createSourceFile("a.js", snippets[1], {});
-console.log(s)
 
 main();
